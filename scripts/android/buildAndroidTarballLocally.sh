274e381ca426802b47f0f1dff5ec7c73778feada
Dominik Sokal <dominiksokal@gmail.com>
2018-07-30 12:00:42 +0200

    [turtle-js] add expokit-npm-package and modules directories to android tarball

--- scripts/android/buildAndroidTarballLocally.sh
+++ scripts/android/buildAndroidTarballLocally.sh
@@ -14,5 +14,7 @@ ln -s ${EXPO_UNIVERSE_DIR}/exponent/__internal__ $TEMP_DIR/__internal__
 ln -s ${EXPO_UNIVERSE_DIR}/exponent/template-files $TEMP_DIR/template-files
 ln -s ${EXPO_UNIVERSE_DIR}/exponent/android $TEMP_DIR/android
 ln -s ${EXPO_UNIVERSE_DIR}/exponent/package.json $TEMP_DIR/package.json
+ln -s ${EXPO_UNIVERSE_DIR}/exponent/expokit-npm-package $TEMP_DIR/expokit-npm-package
+ln -s ${EXPO_UNIVERSE_DIR}/exponent/modules $TEMP_DIR/modules
 cd $TEMP_DIR; tar -czhf $ARTIFACTS_DIR/android-shell-builder.tar.gz .
 rm -rf $TEMP_DIR
