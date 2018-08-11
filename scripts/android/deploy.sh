405fd2faaf5d15f974401e6e72dc7d0124260d43
Nick Novitski <nick@expo.io>
2018-08-10 17:43:31 -0700

    Replace kubernetes deployment scripts

--- scripts/android/deploy.sh
+++ scripts/android/deploy.sh
@@ -1,32 +1,10 @@
 #!/usr/bin/env bash
 
-set -xeo pipefail
+set -xeuo pipefail
 
 environment=$1
 
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
+retry5 gcloud container images add-tag "${IMAGE}:${PREV_TAG}" "${IMAGE}:${CURRENT_TAG}"
+export TAG="${CURRENT_TAG}"
 
-retry5 gcloud container images add-tag $PREV_TAG $CURRENT_TAG
-
-if [ "$environment" == "production" ]; then
-  echo "Deploying Turtle to production..."
-  export REPLICAS=12
-  export ENVIRONMENT=production
-elif [ "$environment" == "staging" ]; then
-  echo "Deploying Turtle to staging..."
-  export REPLICAS=1
-  export ENVIRONMENT=staging
-else
-  echo "Unrecognized environment $environment"
-  exit 1
-fi
-
-if ! retry5 gcloud container images describe "$CURRENT_TAG"; then
-  echo "Unable to find image tagged with $CURRENT_TAG"
-  exit 2
-fi
-
-echo "Environment set, found image, deploying..."
-
-envsubst < $ROOT_DIR/k8s/android/turtle-deployment.template.yml | \
-  kubectl apply --namespace "$environment" --validate=true -f -
+nix run expo.k8s-services.turtle.${environment}.deploy --command deploy
