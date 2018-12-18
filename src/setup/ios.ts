c0745085ec4590dd2fc763c059efcccd3c77cb48
Jakub Stępak <jakub.ste@gmail.com>
2018-12-18 15:03:42 +0100

    [turtle] delete provisioning profiles from homedir (#3344)
    * [turtle] delete provisioning profiles from homedir
    
    * Update server/turtle/src/setup/ios.ts
    
    Co-Authored-By: jakubste <jakub.ste@gmail.com>

--- src/setup/ios.ts
+++ src/setup/ios.ts
@@ -1,9 +1,22 @@
+import os from 'os';
+import path from 'path';
+import fs from 'fs-extra';
+
 import * as keychain from 'turtle/builders/utils/ios/keychain';
 import logger from 'turtle/logger';
 import commonSetup from 'turtle/setup/common';
 
+async function deleteProvisioningProfilesFromHomedir() {
+  const provisioningProfilesDir = path.join(os.homedir(), 'Library/MobileDevice/Provisioning Profiles');
+  const provisioningProfiles = (await fs.readdir(provisioningProfilesDir)).filter(
+    filename => path.extname(filename) === '.mobileprovision',
+  );
+  await Promise.all(provisioningProfiles.map(file => fs.remove(path.join(provisioningProfilesDir, file))));
+}
+
 export default async function setup() {
   logger.info('Setting up environment...');
+  await deleteProvisioningProfilesFromHomedir();
   await keychain.cleanUp();
   await commonSetup();
   logger.info('Finished setting up environment');
