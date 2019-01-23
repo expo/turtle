3dd74ef40653360d7a42f9cdf008a5c00bf703fe
Dominik Sokal <dominik.sokal@swmansion.com>
2019-01-23 17:00:49 +0100

    fix releaseBeta.sh

--- scripts/releaseBeta.sh
+++ scripts/releaseBeta.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
-$ROOT_DIR/node_modules/.bin/release-it -c .release-it.beta.json --preRelease=beta
+$ROOT_DIR/node_modules/.bin/release-it -c $ROOT_DIR/.release-it.beta.json --preRelease=beta
