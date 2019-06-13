98747c1db6b97d89cdd8de925c2b5338d66ffe7b
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2019-06-13 13:15:01 +0200

    fix eventlistener in supervisord

--- scripts/android/supervisordEventListener.sh
+++ scripts/android/supervisordEventListener.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+printf "READY\n"; # signal to superivsord that listener is ready
+
 while read line; do
   echo "[supervisord:eventlistener] Processing Event: $line" >&2;
   kill -SIGQUIT $PPID
