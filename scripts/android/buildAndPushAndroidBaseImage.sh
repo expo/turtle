225c2cd56fc3a090d9c4f94cc26beb035a213aa4
Dominik Sokal <dominik.sokal@swmansion.com>
2020-03-04 11:36:52 +0100

    upgrade ndk

--- scripts/android/buildAndPushAndroidBaseImage.sh
+++ scripts/android/buildAndPushAndroidBaseImage.sh
@@ -3,6 +3,8 @@
 set -eo pipefail
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../.. && pwd )"
+CONFIGURE_ANDROID_SDK_PATH="$ROOT_DIR/scripts/android/configureAndroidSdk.sh"
+DOCKER_BASE_DIR="$ROOT_DIR/docker-base"
 
 IMAGE_TAG="$1"
 if [[ -z "$IMAGE_TAG" ]]; then
@@ -13,10 +15,12 @@ fi
 IMAGE_VERSION_FULL_NAME="gcr.io/exponentjs/turtle-android-base:$IMAGE_TAG"
 IMAGE_LATEST_FULL_NAME="gcr.io/exponentjs/turtle-android-base:latest"
 
-pushd $ROOT_DIR > /dev/null
-docker build -f docker-base/Dockerfile -t "$IMAGE_VERSION_FULL_NAME" -t "$IMAGE_LATEST_FULL_NAME" .
+pushd $DOCKER_BASE_DIR > /dev/null
+cp $CONFIGURE_ANDROID_SDK_PATH .
+docker build -t "$IMAGE_VERSION_FULL_NAME" -t "$IMAGE_LATEST_FULL_NAME" .
 echo "Successfully built $IMAGE_VERSION_FULL_NAME Docker image"
 gcloud docker -- push "$IMAGE_VERSION_FULL_NAME"
 gcloud docker -- push "$IMAGE_LATEST_FULL_NAME"
 echo "Successfully pushed $IMAGE_VERSION_FULL_NAME Docker image to GCloud"
+rm -rf `basename $CONFIGURE_ANDROID_SDK_PATH`
 popd  > /dev/null
