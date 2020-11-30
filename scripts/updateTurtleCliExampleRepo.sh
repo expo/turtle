33f7222ba40f5634991868ae5ff1601d4f7325e6
Brent Vatne <brentvatne@gmail.com>
2020-11-30 13:33:43 -0800

    [scripts] Also verify jq installation in script where it is specifically used

--- scripts/updateTurtleCliExampleRepo.sh
+++ scripts/updateTurtleCliExampleRepo.sh
@@ -2,6 +2,12 @@
 
 set -eo pipefail
 
+if ! command -v jq &> /dev/null
+then
+    echo "jq is not installed and is required by the release script. Install it through homebrew or your package manager of choice."
+    exit
+fi
+
 DEPLOY_ENDPOINT_URL="https://circleci.com/api/v1.1/project/github/expo/turtle-cli-example/tree/updates-do-not-remove-me"
 
 curl -X POST \
